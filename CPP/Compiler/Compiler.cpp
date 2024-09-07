dcb8703ae30c6341aefef33522cd48efe6e56886
Alexey Gutyansky <75169888+AlexixRugis@users.noreply.github.com>
2024-09-07 22:01:37 +0300

    new symbol parse algo

--- CPP/Compiler/Compiler.cpp
+++ CPP/Compiler/Compiler.cpp
@@ -1,5 +1,7 @@
 ﻿#include <iostream>
 #include <fstream>
+#include <vector>
+#include <chrono>
 #include "Lexer.h"
 
 int main()
@@ -7,15 +9,29 @@ int main()
     std::string filePath = "C:\\GitHub_Repos\\PLTest\\input.txt";
     std::ifstream ifs(filePath);
     Lexer lexer(ifs);
+    std::vector<LexerToken> lexems;
+
+    auto start = std::chrono::high_resolution_clock::now();
 
     while (true)
     {
         LexerToken token = lexer.NextToken();
-        std::cout << ToString(token.Type()) << ' ' << token.Value() << std::endl;
+        lexems.push_back(token);
 
         if (token.Type() == LexerTokenType::ENDOFFILE)
         {
             break;
         }
+    }
+
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+
+    std::cout << "Time taken by function: "
+        << duration.count() << " microseconds" << std::endl;
+
+    for (auto& v : lexems)
+    {
+        std::cout << ToString(v.Type()) << ' ' << v.Value() << "\n";
     }
 }
